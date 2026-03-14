5dfe968b2f0842c7f72a567f21a0298a91a205d7
Mac <doremiheev@vk.com>
2026-03-14 12:51:13 +0300

    queue: first try

--- src/main/java/hse/java/lectures/lecture6/tasks/queue/BoundedBlockingQueue.java
+++ src/main/java/hse/java/lectures/lecture6/tasks/queue/BoundedBlockingQueue.java
@@ -1,25 +1,51 @@
 package hse.java.lectures.lecture6.tasks.queue;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BoundedBlockingQueue<T> {
+    int capacity;
+    Queue<T> queue = new LinkedList<T>();
 
 
     public BoundedBlockingQueue(int capacity) {
+        if (capacity <= 0){
+            throw new IllegalArgumentException();
+        }
 
+        this.capacity = capacity;
     }
 
     public void put(T item) {
+        if (item == null){
+            throw  new IllegalArgumentException();
+        }
 
+        while (queue.size() == capacity){
+            try {wait();} catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        queue.offer(item);
     }
 
-    public T take() {
-        return null;
+    public synchronized T take() {
+        while (queue.isEmpty()){
+            try {wait();} catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        T item = queue.poll();
+        notifyAll();
+        return item;
     }
 
-    public int size() {
-        return 0;
+    public synchronized int size() {
+        return queue.size();
     }
 
     public int capacity() {
-        return 0;
+        return capacity;
     }
 }
